42118c22b05e2099f62eb0dc5c954f1f05cf4cf7
to.mo <tomaszjekel@gmail.com>
2023-06-27 05:08:07 +0200

    Update app_mcu_filters.py

--- app_mcu_filters.py
+++ app_mcu_filters.py
@@ -184,7 +184,8 @@ def main():
             server_state["mix_track"] = create_mix_track(
                 kind="video", mixer_callback=mixer_callback, key="mix"
             )
-
+    server_state["mix_track"]="test"
+    
     mix_track = server_state["mix_track"]
 
     self_ctx = webrtc_streamer(
